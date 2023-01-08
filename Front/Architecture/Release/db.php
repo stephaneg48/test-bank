6f21dc67c20e8d555a631b9d2871aa372f5721d1
Stephane Gilles <sag48@njit.edu>
2023-01-07 19:05:26 -0500

    small fix to previous commit

--- Front/Architecture/Release/db.php
+++ Front/Architecture/Release/db.php
@@ -7,19 +7,18 @@ $ini = @parse_ini_file("lib/.env");
 if($ini && isset($ini["JAWSDB_URL"])){
     //load local .env file
     $db_url = parse_url($ini["JAWSDB_URL"]);
-    $db_url["path"] = ltrim($db_url["path"], "/");
+    //$db_url["path"] = ltrim($db_url["path"], "/");
 }
 else{
     //load from heroku env variables
     $db_url = parse_url(getenv("JAWSDB_URL"));
-    $db_url["path"] = ltrim($db_url["path"], "/"); // must trim for heroku
+    //$db_url["path"] = ltrim($db_url["path"], "/"); // don't need to trim for jawsdb?
 }
 
 $db_host = $db_url["host"]; // server hosting database
 $db_user = $db_url["user"]; // name of user account
 $db_name = substr($db_url["path"],1); // name of database from user account
 $db_password = $db_url["pass"];
-
 
 $db = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
@@ -35,6 +34,7 @@ else
 
 
 mysqli_select_db($db, $db_name); // makes db_name the default
+
 
 ?>
 
